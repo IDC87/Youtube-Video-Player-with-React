672370779bd4d3a7963b033408eefc7d9b02c7d9
IDC87 <ivo.diogo@gmail.com>
2023-01-26 15:11:40 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# PUSH_SWAP
+
+<b>A simple application that fetch information from youtube api and displays its videos with React and Babel support from Redux</b> 
+
+1 . <tt>clone repo and cd into it</tt>
+
+2 . <tt>npm start and go to localhost:8080 in the web browser </tt>
+
+![](push_swap_leaker_tester_demonstration_updated.gif)
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTsearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+//import youtubeApiSearch from 'youtube-api-search';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyBL1STwyXm2OJNAFYoKVcF2PTfunEYsCvg';
+
+class App extends Component  {
+	constructor(props) {
+		super(props);
+
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch("Lisboa");
+		
+	}
+	videoSearch(term) {
+		YTsearch({key: API_KEY, term: term }, (videos) => {
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			}); 
+		});
+	}
+	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+		return (
+		<div>
+			<SearchBar onSearchTermChange = { videoSearch }/>
+			<VideoDetail video={this.state.selectedVideo}/>
+			<VideoList 
+			onVideoSelect = { selectedVideo => this.setState({selectedVideo}) }
+			videos={this.state.videos} />
+		</div>
+		);
+	}	
+}
+ReactDOM.render(<App />, document.querySelector('.container'));
